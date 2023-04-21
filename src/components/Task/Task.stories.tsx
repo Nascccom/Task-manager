b72c27779aff37e1270ab31d65700247082144e1
Nascccom <ostrov_o@bk.ru>
2023-04-21 15:54:06 +0300

    Task.stories.tsx

--- src/components/Task/Task.stories.tsx
+++ src/components/Task/Task.stories.tsx
@@ -1,10 +1,17 @@
-import type {Meta} from '@storybook/react';
+import type {ComponentStory, Meta} from '@storybook/react';
 import {Task} from "./Task";
 import {ReduxStoreProviderDecorator} from "../../state/ReduxStoreProviderDecorator/ReduxStoreProviderDecorator";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {TaskType} from "../Todolist/ToDoList";
 import {AppRootStateType} from "../../state/store/store";
 import React from 'react';
+import {SuperCheckBox} from "../SuperCheckBox/SuperCheckBox";
+import {EditableSpan} from "../EditableSpan/EditableSpan";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import styles from "../Todolist/Todolist.module.css";
+import {changeToggleTaskAC, removeTaskAC, updateTaskAC} from "../../state/reducers/task-reducer";
+import {action} from '@storybook/addon-actions';
 
 
 const meta: Meta<typeof Task> = {
@@ -12,33 +19,61 @@ const meta: Meta<typeof Task> = {
     component: Task,
     tags: ['autodocs'],
     decorators: [ReduxStoreProviderDecorator],
+    parameters: {
+        actions: {argTypesRegex: '^on.*'},
+    },
 
 }
-
-const TaskReduxIsDone = () => {
-    const task = useSelector<AppRootStateType, TaskType>(
-      state => state.tasks['todolistId2'][1])
-    return <Task task={task} todolistId={'todolistId2'}/>
-}
-const TaskReduxNotIsDone = () => {
-    const task = useSelector<AppRootStateType, TaskType>(
-      state => state.tasks['todolistId1'][1])
-    return <Task task={task} todolistId={'todolistId1'}/>
-}
-
 export default meta;
 
-const Template1 = () => <TaskReduxIsDone  />
-export const TaskIsDone = Template1.bind({});
-// @ts-ignore
-TaskIsDone.args =  {
+type TaskRedux = {
+    todolistId: string
+}
 
+const TaskRedux = ({todolistId}: TaskRedux) => {
+    // const todolistId = 'todolistId2'
+    const task = useSelector<AppRootStateType, TaskType>(
+      state => state.tasks[todolistId][1])
+    const dispatch = useDispatch()
+
+    const removeTaskHandler = () => {
+        dispatch(removeTaskAC(todolistId, task.id))
+    }
+
+    const updateTaskTitleHandler = (newTitle: string) => {
+        dispatch(updateTaskAC(todolistId, task.id, newTitle))
+    }
+
+    const changeCheckboxStatus = (checked: boolean) => {
+        dispatch(changeToggleTaskAC(todolistId, task.id, checked))
+    }
+
+
+    return (
+      <li className={task.isDone ? styles.isDone : ''}>
+          <SuperCheckBox callBack={(checked) => changeCheckboxStatus(checked)}
+                         checked={task.isDone}/>
+
+          <EditableSpan title={task.title}
+                        callBack={updateTaskTitleHandler}/>
+
+          <IconButton aria-label="delete"
+                      onClick={action('Removed task')}>
+              <DeleteIcon/>
+          </IconButton>
+      </li>
+    )
 }
 
 
-const Template2 = () => <TaskReduxNotIsDone  />
-export const TaskNotIsDone = Template2.bind({});
-// @ts-ignore
-TaskNotIsDone.args =  {
+const Template1: ComponentStory<typeof TaskRedux> = (args) => <TaskRedux {...args} />
 
+export const TaskIsDone = Template1.bind({});
+TaskIsDone.args = {
+    todolistId: 'todolistId2'
+}
+
+export const TaskNotIsDone = Template1.bind({});
+TaskNotIsDone.args = {
+    todolistId: 'todolistId1'
 }
