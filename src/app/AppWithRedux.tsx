94e3535b0757cb2fbfa94d5a827a31163043fe9a
Nascccom <ostrov_o@bk.ru>
2023-10-22 16:01:19 +0300

    Added react-router-dom, formik and configured routes

--- src/app/AppWithRedux.tsx
+++ src/app/AppWithRedux.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {ButtonAppBar} from '../features/ButtonAppBar/ButtonAppBar';
 import Container from '@mui/material/Container';
@@ -7,9 +7,19 @@ import LinearProgress from '@mui/material/LinearProgress';
 import {useAppSelector} from "../hooks/useSelector/useSelector";
 import {RequestStatusType} from "./app-reducer";
 import {ErrorSnackbars} from "../components/ErrorSnackbar/ErrorSnackbar";
+import {Login} from "../features/Login/Login";
+import {Navigate, Route, Routes} from "react-router-dom";
+import {useAppDispatch} from "../hooks/useDiapstch/useDispacth";
+import {getAuthMeDataTC} from "../state/reducers/auth-reducer";
 
 export function AppWithRedux() {
+    const isAuth = useAppSelector(state => state.auth.isAuth)
     const status = useAppSelector<RequestStatusType>(state => state.app.status)
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(getAuthMeDataTC())
+    }, [dispatch])
 
     return (
       <div className="App">
@@ -18,10 +28,16 @@ export function AppWithRedux() {
           {status === 'loading' && <LinearProgress color={'secondary'}/>}
 
           <Container fixed maxWidth={false}>
-              <TodolistList/>
+              <Routes>
+                  <Route path={'/'} element={<TodolistList/>}/>
+                  <Route path={'/login'} element={<Login/>}/>
+                  <Route path={'/404'} element={<h1>404: PAGE NOT FOUND</h1>}/>
+                  <Route path={'*'} element={<Navigate to={'/404'}/>}/>
+              </Routes>
           </Container>
 
-         <ErrorSnackbars />
+          <ErrorSnackbars/>
+
       </div>
     );
 }
