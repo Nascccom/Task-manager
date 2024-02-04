72c4ca549bbda4d40441c73dfbf194f3af5d28c6
Nascccom <ostrov_o@bk.ru>
2024-02-04 22:13:03 +0300

    Created styles variable and refactoring

--- src/common/components/EntryField/style.ts
+++ src/common/components/EntryField/style.ts
@@ -4,11 +4,15 @@ import { CSSProperties } from "react"
 export const style = {
     inputField: (formik: FormikProps<{ title: string }>) =>
         ({
-            color: formik.errors.title && "#cb0b0b",
+            color: formik.errors.title && "var(--errorColor)",
         }) as CSSProperties,
     button: (formik: FormikProps<{ title: string }>, disabled: boolean | undefined) =>
         ({
-            color: formik.errors.title ? "#cb0b0b" : disabled ? "#888888" : "#1976d2",
+            color: formik.errors.title
+                ? "var(--errorColor)"
+                : disabled
+                ? "var(--disabledColor)"
+                : "var(--primaryColor)",
             marginLeft: "10px",
         }) as CSSProperties,
 }
